05994339d4f7ae07e484ef6f58f63bd2a8a02dab
VirtualTorus <37066707+VirtualTorus@users.noreply.github.com>
2019-03-06 14:25:33 -0500

    fire station sources update

--- education_facility/opentab_scripts/on/pre-on-private.sh
+++ education_facility/opentab_scripts/on/pre-on-private.sh
@@ -3,7 +3,7 @@
 RAW_DIR="./pddir/raw"
 XLSX_FILE="$(echo $1 | sed 's/.csv/.xlsx/')"
 
-cp $1 ${XLSX_FILE}
+mv $1 ${XLSX_FILE}
 
 libreoffice --headless --convert-to csv ${XLS_FILE} --outdir ${RAW_DIR}
 
