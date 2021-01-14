a0db33b7db878502a718d8f151dd06b9688e3178
Joseph Kuchar <joseph.kuchar@gmail.com>
2021-01-14 10:36:09 -0500

    Add files via upload

---
+++ scripts/Addresses/process_kingston.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 09:12:56 2021
+
+@author: Joseph Kuchar (joseph.kuchar@canada.ca)
+
+This just converts a geojson downloaded from the Vancouver open data portal to a shapefile,
+as the openaddresses processing script struggled with the formatting of the geojson.
+"""
+
+import geopandas as gpd
+
+gdf=gpd.read_file(r"C:\Users\josep\OneDrive\Work\ODA\pre-processing\kingston-civic-addresses.geojson")
+gdf.to_file('kingston-civic-addresses.shp')
+
